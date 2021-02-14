1f901aa87deca107ea382168426f03b1c6221471
MrMagawly <mr_magawly@protonmail.com>
2021-02-14 16:58:09 -0500

    Set Scroll Container set_value() to the max value when the size of the ChatBox changes.

--- UI/ScrollContainer.gd
+++ UI/ScrollContainer.gd
@@ -4,17 +4,12 @@ var v_bar
 
 func _ready():
 	v_bar = get_v_scrollbar()
+	
 
 func _on_ChatBox_item_added():
-	#v_bar.set_value(v_bar.get_max())
 	pass
 
 
 func _on_ChatBox_resized():
-	#v_bar.set_value(v_bar.get_max())
-	pass
+	v_bar.set_value(v_bar.get_max())
 
-
-func _on_ChatBox_item_rect_changed():
-	#v_bar.set_max()
-	pass
