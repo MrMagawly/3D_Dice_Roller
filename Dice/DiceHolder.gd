e0633ac5d9e7c7b76cdcfd78893c65a0c0c90bf1
MrMagawly <mr_magawly@protonmail.com>
2021-02-25 00:10:27 -0500

    Add ability to spawn more dice on the board. Not fully functioning.

--- Dice/DiceHolder.gd
+++ Dice/DiceHolder.gd
@@ -38,38 +38,6 @@ func _ready():
 	dice_finished_count = 0
 	dice_results = ""
 	
-#	var count = 0
-#	while count < 1:
-#		var dice_one = d_boost_load.instance()
-#		var dice_two = d_setback_load.instance()
-#		var dice_three = d_ability_load.instance()
-#		var dice_four = d_difficulty_load.instance()
-#		var dice_five = d_challenge_load.instance()
-#		var dice_six = d_proficiency_load.instance()
-		#var dice_seven = d_force_load.instance()
-		#dice_one.connect("finished_rolling", self, "_on_dice_finished_rolling")
-		#dice_two.connect("finished_rolling", self, "_on_dice_finished_rolling")
-#		dice_three.connect("finished_rolling", self, "_on_dice_finished_rolling")
-#		dice_four.connect("finished_rolling", self, "_on_dice_finished_rolling")
-		#dice_five.connect("finished_rolling", self, "_on_dice_finished_rolling")
-		#dice_six.connect("finished_rolling", self, "_on_dice_finished_rolling")
-		#dice_seven.connect("finished_rolling", self, "_on_dice_finished_rolling")
-		#add_child(dice_one)
-		#add_child(dice_two)
-#		add_child(dice_three)
-#		add_child(dice_four)
-		#add_child(dice_five)
-		#add_child(dice_six)
-		#add_child(dice_seven)
-		#dice_one.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
-		#dice_two.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
-#		dice_three.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
-#		dice_four.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
-		#dice_five.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
-		#dice_six.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
-		#dice_seven.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
-#		count += 1
-	
 	die = get_children()
 
 func _on_dice_finished_rolling(result, type):
@@ -91,8 +59,23 @@ func _on_dice_finished_rolling(result, type):
 
 
 func _on_ChatTerminal_got_dice_to_spawn(regular_dice, narrative_dice, fate_dice):
-	for i in range(narrative_dice.Boost):
-		var dice_one = d_boost_load.instance()
-		dice_one.connect("finished_rolling", self, "_on_dice_finished_rolling")
-		add_child(dice_one)
-		dice_one.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
+	spawn_dice(d_six_load.instance(), regular_dice.D6)
+	spawn_dice(d_eight_load.instance(), regular_dice.D8)
+	spawn_dice(d_ten_load.instance(), regular_dice.D10)
+	spawn_dice(d_twelve_load.instance(), regular_dice.D12)
+	spawn_dice(d_twenty_load.instance(), regular_dice.D20)
+	#spawn_dice(d_hundred_load.instance(), regular_dice.D100)
+	spawn_dice(d_boost_load.instance(), narrative_dice.Boost)
+	spawn_dice(d_ability_load.instance(), narrative_dice.Ability)
+	spawn_dice(d_proficiency_load.instance(), narrative_dice.Proficiency)
+	spawn_dice(d_setback_load.instance(), narrative_dice.Setback)
+	spawn_dice(d_difficulty_load.instance(), narrative_dice.Difficulty)
+	spawn_dice(d_challenge_load.instance(), narrative_dice.Challenge)
+	#spawn_dice(d_force_load.instance(), narrative_dice.Force)
+	#spawn_dice(d_fate_load.instance(), fate_dice)
+
+func spawn_dice(dice, count):
+	for i in range(count):
+		dice.connect("finished_rolling", self, "_on_dice_finished_rolling")
+		add_child(dice)
+		dice.translate(Vector3(rand_range(1,15),rand_range(1,5),rand_range(1,15)))
